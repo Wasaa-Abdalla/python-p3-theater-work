c2cfc10fb10e3f49495fb26d3c03ab1e39c324a5
abdalla-wasaa <abdallawasaa7@gmail.com>
2024-02-15 11:53:21 +0300

    Attempted

---
+++ lib/migrations/versions/d074a65912e9_added_audition_and_role_table_classes.py
@@ -0,0 +1,24 @@
+"""Added Audition and Role Table Classes
+
+Revision ID: d074a65912e9
+Revises: 
+Create Date: 2024-02-15 11:45:11.810091
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd074a65912e9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,3 +8,30 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer(), primary_key=True)
+    actor = Column(String())
+    location = Column(String())
+    phone = Column(Integer())
+    hired = Column(Boolean())
+    role_id = relationship('Role', backref=backref('audition'), cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'Audition(id={self.id}, ' + \
+            f'actor={self.actor}, ' + \
+            f'role_id={self.role_id})'
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer(), primary_key=True)
+    character_name = Column(String())
+    audition_id = Column(Integer(), ForeignKey('auditions.id'))
+
+    def __repr__(self):
+        return f'Role(id={self.id}, ' + \
+            f'character_name={self.character_name}, ' + \
+                f'audition_id={self.audition_id})'
